68a893022b0082cd8f5dca030c7b780be04c82d9
Abhishek-ai-ml <abhishekdoulia@gmail.com>
2024-03-24 20:48:44 +0530

    carousel functionality updation

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -4,68 +4,46 @@ import './carousel.css'
 import { calculateNewValue } from '@testing-library/user-event/dist/utils';
 
 const Carousel = ({images}) => {
-    const [displayedImages, setDisplayedImages] = useState(images.slice(0, 5));
+    const [carouselImages, setCarourselImages]= useState(images);
     const [currentIndex, setCurrentIndex] = useState(2);
-    const [windowSize, setWindowSize] = useState({
-        width: window.innerWidth,
-        height: window.innerHeight
-    });
-
-    useEffect(() => {
-        const handleResize = () => {
-            setWindowSize({
-                width: window.innerWidth,
-                height: window.innerHeight
-            });
-        };
-    
-        window.addEventListener('resize', handleResize);
-        return () => {
-          window.removeEventListener('resize', handleResize);
-        };
-    }, []);
 
     const handleLeft =() => {
-        if (images.length > 1) { 
-            var firstImage = images.shift(); 
-            images.push(firstImage); 
-        }
+        const carousel = [...carouselImages];
+        const firstImage = carousel.shift(); 
+        carousel.push(firstImage); 
+        setCarourselImages(carousel)
     }
 
     const handleRight = () => {
-        if (images.length > 1) { 
-            var lastImage = images.pop(); 
-            images.unshift(lastImage); 
-        }
+        const carousel = [...carouselImages];
+        const lastImage = carousel.pop(); 
+        carousel.unshift(lastImage); 
+        setCarourselImages(carousel)
     }
-
-    useEffect(() => {
-       setDisplayedImages(images.slice(0, 5)); 
-    }, [displayedImages])
 
   return (
     <div className='carousel-container'>
         {/* Images */}
         <div className='carousel-image-section'>
             <div className='carousel-image-stepTwo-left'>
-                <img src={displayedImages[0].img}/>
+                <img src={carouselImages[0].img}/>
             </div>
 
             <div className='carousel-image-stepOne-left'>
-                <img src={displayedImages[1].img}/>
+                <img src={carouselImages[1].img}/>
             </div>
 
             <div className='carousel-center-image'>
-                <img src={displayedImages[2].img}/>
+                <img src={carouselImages[2].img}/>
                 <div>Sample text here</div>
             </div>
 
             <div className='carousel-image-stepOne-right'>
-                <img src={displayedImages[3].img}/>
+                <img src={carouselImages[3].img}/>
             </div>
 
             <div className='carousel-image-stepTwo-right'>
-                <img src={displayedImages[4].img}/>
+                <img src={carouselImages[4].img}/>
             </div>
         </div>
 
@@ -74,7 +52,7 @@ const Carousel = ({images}) => {
             <div className='pagination-left-arrow' onClick={() => handleLeft()}><HiArrowSmLeft size={24}/></div>
             <div className='pagination-dot'>
                 {
-                    images.map((i) => (
+                    carouselImages.map((i) => (
                         <div className={currentIndex === i.index ? "selected-dot" : "dot"}></div>
                     ))
                 }
